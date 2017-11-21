28a33925cbef0fda08c0a92d7a2c5c0962a9262b
jie cai <jc926@afsconnect1.njit.edu>
2017-11-21 15:44:12 -0500

    all class test

--- htmltable.php
+++ htmltable.php
@@ -1,19 +1,29 @@
 <?php
 class htmltable{
     public static function Atable($input){
+       
         $table = htmltag:: tablestart();
+        foreach ($input as $row =>$line){
+            $table.= htmltag:: rowstart();
+            foreach ($line as $inrow =>$value){
+                $table.=htmltag::tablehead($inrow);
+            }
+            $table.=htmltag:: rowend();
+            break 1;
+        }
         foreach ($input as $row =>$line){
             $table.= htmltag:: rowstart();
             foreach ($line as $inrow =>$value){
                 $table.=htmltag::tabledata($value);
             }
-            $table.=thmltag:: rowend();
+            $table.=htmltag:: rowend();
         }
-        $table = htmltag::tableend();
+        $table .= htmltag::tableend();
         return $table;   
 
     }
 
 }
+
 
 ?>
--- index.php
+++ index.php
@@ -14,4 +14,22 @@ class Manage {
 spl_autoload_register(array('Manage', 'autoload'));
 //instantiate the program object
 $obj = new main();
+
+class main{
+	public function _construct(){
+		$from = '<form method ="post" enctype= "multipart/form-data">';
+	    $form .='<h1>find all todos records</h1>';
+	    $allT= todos::findAll();
+	  	$tableshow = htmltable:: Atable($allT);
+		$form .=$tableshow;
+		$form .= '</form>';
+		print($form);
+
+
+	}
+
+
+}
+
+
 ?>
